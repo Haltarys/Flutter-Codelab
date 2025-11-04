591c90f6a424d17937e669fcb8a078621d38d49a
Haltarys <45515869+Haltarys@users.noreply.github.com>
2025-11-04 17:55:35 +0100

    feat(app): show snackbar toast when adding & removing from favourites

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,23 @@ import 'package:flutter_codelab/home.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final _scaffoldKey = GlobalKey<ScaffoldMessengerState>();
 
   // This widget is the root of your application.
   @override
   Widget build(final BuildContext context) {
     return ChangeNotifierProvider(
-      create: (final context) => MyAppState(),
+      create: (final context) => MyAppState(_scaffoldKey),
       child: MaterialApp(
         title: 'Namer App',
         theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.red)),
+        scaffoldMessengerKey: _scaffoldKey,
         home: const Home(),
       ),
     );
@@ -25,6 +28,10 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
+  MyAppState(this._scaffoldKey);
+
+  final GlobalKey<ScaffoldMessengerState> _scaffoldKey;
+
   var current = WordPair.random();
 
   void getNext() {
@@ -34,18 +41,29 @@ class MyAppState extends ChangeNotifier {
 
   var favourites = <WordPair>{};
 
+  void _showFavouritedToast(final String message) {
+    _scaffoldKey.currentState!.showSnackBar(
+      SnackBar(content: Text(message), duration: const Duration(seconds: 1)),
+    );
+  }
+
   void toggleFavourite() {
     if (favourites.contains(current)) {
       favourites.remove(current);
+      _showFavouritedToast('"${current.asLowerCase}" was removed from favourites.');
     } else {
       favourites.add(current);
+      _showFavouritedToast('"${current.asLowerCase}" was added to favourites.');
     }
     print('Favourites: $favourites');
+
     notifyListeners();
   }
 
   void removeFavourite(final WordPair favourite) {
     favourites.remove(favourite);
+    _showFavouritedToast('"${current.asLowerCase}" was removed from favourites.');
+
     notifyListeners();
   }
 }
