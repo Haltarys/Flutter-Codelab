6aced910584cf5011287692c10ebe70d08da360a
Haltarys <45515869+Haltarys@users.noreply.github.com>
2025-11-07 13:47:31 +0100

    feat(app): persist current & favourited word pairs in storage

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,27 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_codelab/home.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferencesWithCache.create(
+    cacheOptions: const SharedPreferencesWithCacheOptions(),
+  );
+  runApp(MyApp(prefs: prefs));
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  MyApp({super.key, required this.prefs});
 
+  final SharedPreferencesWithCache prefs;
   final _scaffoldKey = GlobalKey<ScaffoldMessengerState>();
 
   // This widget is the root of your application.
   @override
   Widget build(final BuildContext context) {
     return ChangeNotifierProvider(
-      create: (final context) => MyAppState(_scaffoldKey),
+      create: (final context) => MyAppState(_scaffoldKey, prefs),
       child: MaterialApp(
         title: 'Namer App',
         theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.red)),
@@ -28,23 +34,50 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  MyAppState(this._scaffoldKey);
-
-  final GlobalKey<ScaffoldMessengerState> _scaffoldKey;
-
-  var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
+  MyAppState(this._scaffoldKey, this._prefs) {
+    _loadState();
   }
 
-  var favourites = <WordPair>{};
+  final GlobalKey<ScaffoldMessengerState> _scaffoldKey;
+  final SharedPreferencesWithCache _prefs;
+
+  late WordPair current;
+  late Set<WordPair> favourites;
+
+  void _loadState() {
+    final (storedFirst, storedSecond) = (_prefs.getString('first'), _prefs.getString('second'));
+    print('$storedFirst, $storedSecond');
+    current = storedFirst == null || storedSecond == null
+        ? WordPair.random()
+        : WordPair(storedFirst, storedSecond);
+
+    final storedFavourites = _prefs.getStringList('favourites');
+    print('$storedFavourites');
+    favourites = storedFavourites == null || storedFavourites.isEmpty
+        ? <WordPair>{}
+        : storedFavourites.map((final s) {
+            final [first, second] = s.split('|');
+            return WordPair(first, second);
+          }).toSet();
+  }
 
   void _showFavouritedToast(final String message) {
     _scaffoldKey.currentState!.showSnackBar(
       SnackBar(content: Text(message), duration: const Duration(seconds: 1)),
     );
+  }
+
+  List<String> serialiseFavourites() {
+    return favourites.map((final pair) => '${pair.first}|${pair.second}').toList();
+  }
+
+  void getNext() {
+    current = WordPair.random();
+
+    _prefs.setString('first', current.first); // Dangling Future
+    _prefs.setString('second', current.second); // Dangling Future
+
+    notifyListeners();
   }
 
   void toggleFavourite() {
@@ -55,15 +88,16 @@ class MyAppState extends ChangeNotifier {
       favourites.add(current);
       _showFavouritedToast('"${current.asLowerCase}" was added to favourites.');
     }
-    print('Favourites: $favourites');
 
+    _prefs.setStringList('favourites', serialiseFavourites());
     notifyListeners();
   }
 
   void removeFavourite(final WordPair favourite) {
     favourites.remove(favourite);
-    _showFavouritedToast('"${current.asLowerCase}" was removed from favourites.');
+    _showFavouritedToast('"${favourite.asLowerCase}" was removed from favourites.');
 
+    _prefs.setStringList('favourites', serialiseFavourites());
     notifyListeners();
   }
 }
