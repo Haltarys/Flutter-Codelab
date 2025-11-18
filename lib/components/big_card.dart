8d67d541e839336326a58e71dc87394867e87c1a
Haltarys <45515869+Haltarys@users.noreply.github.com>
2025-11-18 16:17:01 +0100

    refactor(big_card): tweak card's semantic label

--- lib/components/big_card.dart
+++ lib/components/big_card.dart
@@ -19,7 +19,7 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase, style: style, semanticsLabel: '${pair.first} ${pair.second}'),
+        child: Text(pair.asLowerCase, style: style, semanticsLabel: pair.asPascalCase),
       ),
     );
   }
